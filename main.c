28c39a528a3873ee2ad79864896a4d6da5d316a7
Daniel Rahme <daniel.rahme.dr@gmail.com>
2021-03-15 15:42:27 +0100

    Added cache-hit and miss functions

--- main.c
+++ main.c
@@ -4,6 +4,35 @@
 #include "nrf_delay.h"
 #include "boards.h"
 
+void cache_enable()
+{
+    const int enable_bit = (1 << 0);
+    const int profiler_enable_bit = (1 << 8);
+
+    NRF_NVMC->ICACHECNF = enable_bit | profiler_enable_bit;
+}
+
+void cache_disable()
+{
+    NRF_NVMC->ICACHECNF = 0x0;
+}
+
+int cache_hit()
+{
+    return NRF_NVMC->IHIT;
+}
+
+int cache_miss()
+{
+    return NRF_NVMC->IMISS;
+}
+
+void cache_hit_miss_reset()
+{
+    NRF_NVMC->IHIT  = 0x0;
+    NRF_NVMC->IMISS = 0x0;
+}
+
 void toggle_leds()
 {
     for (int i = 0; i < LEDS_NUMBER; i++)
@@ -16,6 +45,16 @@ void toggle_leds()
 int main(void)
 {
     bsp_board_init(BSP_INIT_LEDS);
+    cache_enable();
+
+    cache_disable();
+
+    cache_hit_miss_reset();
+
+    if (cache_hit() > 0) {
+        while(true) 
+            toggle_leds();
+    }
 
     while (true)
     {
