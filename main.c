a33e740581260232a9fd69e81f26546d158910e3
Daniel Rahme <daniel.rahme.dr@gmail.com>
2021-05-20 13:34:03 +0200

    Final cleanup

--- main.c
+++ main.c
@@ -9,6 +9,7 @@
 #include "arm_math.h"
 #include "arm_const_structs.h"
 
+// Enable this to run from RAM. Make sure to use the correct linker-script.
 #define RUN_FROM_RAM 1
 
 #define TEST_LENGTH_SAMPLES 2048
@@ -30,31 +31,6 @@ void fft_test()
     arm_cfft_f32(&arm_cfft_sR_f32_len1024, testInput_f32_10khz, ifftFlag, doBitReverse);
 }
 
-void toggle_leds()
-{
-    for (int i = 0; i < LEDS_NUMBER; i++)
-    {
-        bsp_board_led_invert(i);
-        nrf_delay_ms(500);
-    }
-}
-
-void toggle_test_led()
-{
-    bsp_board_led_invert(0);
-}
-
-void blink_start_seq()
-{
-    const int delay = 250;
-    for (int i = 0; i < 3; i++) {
-        bsp_board_led_on(0);
-        nrf_delay_ms(delay);
-        bsp_board_led_off(0);
-        nrf_delay_ms(delay);
-    }
-}
-
 
 int main(void)
 {
@@ -67,15 +43,15 @@ int main(void)
     extern unsigned char __my_cmsis_dsp_start__[];
     extern unsigned char __my_cmsis_dsp_end__[];
 
-    /* Copy image from flash to RAM. */
+    // Copy image from flash to RAM.
     memcpy(__my_cmsis_dsp_start__,
           __my_cmsis_dsp_load_start__,
           __my_cmsis_dsp_end__ - __my_cmsis_dsp_start__);
 #endif
 
     const int test_samples = 1000;
-    bsp_board_init(BSP_INIT_LEDS);
     const int delay = 250;
+    bsp_board_init(BSP_INIT_LEDS);
 
     // Start sequence
     for (int i = 0; i < 5; i++) {
@@ -89,7 +65,6 @@ int main(void)
         fft_test();
     }
 
-
     // Start sequence
     for (int i = 0; i < 5; i++) {
         bsp_board_led_invert(0);
@@ -99,16 +74,18 @@ int main(void)
     // Enable cache
     cache_enable();
 
-    // Run FFT with cache enabled
     bsp_board_led_invert(0);
+
+    // Run FFT with cache enabled
     for (int i = 0; i < test_samples; i++) {
         fft_test();
     }
 
-    // End
+    // End sequence
     bsp_board_led_invert(0);
 
     while (true)
     {
+        // Do nothing
     }
 }
